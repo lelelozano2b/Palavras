8ece6e79e694200298e37a5bc2789c28f52f3d45
lelelozano2b <00001116727365sp@al.educacao.sp.gov.br>
2024-06-05 11:47:30 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,37 @@
+let palavra;
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
-  function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background("white");
-    fill("black");
-    textSize(64);
-    textAlign(CENTER, CENTER)
-    
-    let maximo = width;
-    let minimo = 0;
-    let palavra = "Lele";
-    let quantidade = map(mouseX, 0, width, 1, palavra.length);
-    let parcial = palavra.substring(0,quantidade);
-    text(parcial,200,200);
-    
-  //  if(mouseX < 50){
-  //    let palavra = "C";
-  //    text(palavra, 200, 200);
-  //  } else {
-  //    let palavra = "Lele";
-  //    text(palavra, 200, 200);
-  //  }
-  }
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria(){
+  let palavras = ["Corinthiana", "por", "amor"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+
+  inicializaCores();
+
+  let texto = palavraParcial(0, width);
+  text(texto, 200, 200);
+}
+
   
   
